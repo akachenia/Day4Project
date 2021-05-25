939755bb833e0892a747e2ffba7a1add5bdff7c2
akachenia <akachenia@gmail.com>
2021-05-25 19:51:30 -0400

    added comments

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,9 @@
 public class BranchingPractice {
     public static void main(String[] args) {
-
+        //created a new project
+        // enable version control
+        //added .gitignore with .idea, taget folder and .iml file
+        //made initial by keeping track pf all files
+        //shared project on github
     }
 }
