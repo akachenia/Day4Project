1f013a293d40da7205b47e7ffe5d841267519e54
akachenia <akachenia@gmail.com>
2021-05-25 19:23:40 -0400

    shared on github

---
+++ src/test/java/BranchingPracticing.java
@@ -0,0 +1,11 @@
+public class BranchingPracticing {
+    public static void main(String[] args) {
+
+        // created new project
+        //enabled version control
+        // added .gitignore with.idea, target folder and .iml file
+        // made initial comit by keeping track of all files
+        //shared project on github
+
+    }
+}
